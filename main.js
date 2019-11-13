6b446f7d019c3e75bef667d77cd77679b67d6ec8
Joao Freitas <joaovotk@gmail.com>
2019-11-13 00:17:48 -0300

    Testando Herança ES6

--- main.js
+++ main.js
@@ -1,3 +1,18 @@
+class List {
+  constructor(){
+    this.data = [];
+  }
+
+  add(data){
+    this.data.push(data);
+    console.log(this.data);
+  }
+}
+
+class MyHClass extends List{
+
+}
+
 class TodoList {
   constructor(){
     this.todos = []
@@ -10,7 +25,12 @@ class TodoList {
 }
 
 const MinhaLista = new TodoList();
+const MinhaHeranca = new MyHClass();
+
+// document.getElementById('novotodo', onclick = function(){
+//   MinhaLista.addTodo();
+// })
 
 document.getElementById('novotodo', onclick = function(){
-  MinhaLista.addTodo();
+  MinhaHeranca.add('Minha Herança');
 })
